a91f218e3af15fcf5b67d5181b63d0a9f7aa7a07
ErezLDJ <erezldj@gmail.com>
2020-10-04 11:08:09 +0300

    netlify2 git commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LOL Erexz Coijn
+          Link to Test
         </a>
       </header>
     </div>
